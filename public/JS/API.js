c445e59520a5acc238e17ef7eb8a84d8d3822294
unknown <mateus.saraiva09@gmail.com>
2023-01-04 17:54:19 -0300

    API

---
+++ public/JS/API.js
@@ -0,0 +1,7 @@
+let api_key = "b7c257791c7c9a9552fbce87526bb019"
+
+let img_url = "https://image.tmdb.org/t/p/w500"
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
